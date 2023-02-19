d1199fcfb72aa4290af1ed9eaaf0612cfb039dd5
Chip Thrasher <thechipthrasher@gmail.com>
2023-02-19 17:28:23 -0500

    Change to iOS based project

--- Driving LogTests/Driving_LogTests.swift
+++ Driving LogTests/Driving_LogTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Driving_Log
 
 final class Driving_LogTests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class Driving_LogTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
