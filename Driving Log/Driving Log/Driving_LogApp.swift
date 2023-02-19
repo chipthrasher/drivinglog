8366baeb643b90a07d252b64c3d081924f4963df
Chip Thrasher <thechipthrasher@gmail.com>
2023-02-19 17:10:57 -0500

    First commit

---
+++ Driving Log/Driving Log/Driving_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Driving_LogApp.swift
+//  Driving Log
+//
+//  Created by Chip on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Driving_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
